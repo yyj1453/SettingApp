137b8421b4063805979806b12ec890bc66bdcbc0
yyj1453 <112950093+yyj1453@users.noreply.github.com>
2023-11-30 14:02:06 +0900

    Initial Commit

---
+++ SettingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SettingApp
+//
+//  Created by 윤영진 on 11/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SettingApp/SettingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingAppApp.swift
+//  SettingApp
+//
+//  Created by 윤영진 on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SettingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
